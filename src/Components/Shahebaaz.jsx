744286d81a3727e79713d166de54b107ec8ccec2
sayyed.shahebaaz <sayyed.shahebaaz@datamatics.com>
2024-10-17 12:04:01 +0530

    new updates 2

--- src/Components/Shahebaaz.jsx
+++ src/Components/Shahebaaz.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Shahebaaz() {
     return (
       <div>
-        <h1>Welcome to my component broooooooo.....</h1>
+        <h1>well come</h1>
       </div>
     );
   }
